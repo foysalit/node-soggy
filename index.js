3d4b404bab82ab587e25ba764fb0337c6a2cec56
Foysal Ahamed <cfaion341@gmail.com>
2016-02-07 22:45:22 +0100

    scraping works

---
+++ index.js
@@ -0,0 +1,40 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var express = require('express');
+var app = express();
+
+require('dotenv').config({silent: true});
+
+app.get('/', function (req, res) {
+	var result = {
+		error: false,
+		data: null
+	};
+
+	var url = "http://questure.poliziadistato.it/stranieri/?mime=1&lang="+ req.query.language +"&pratica="+ req.query.receipt;
+
+	request(url, function(error, response, html){
+        if(error){
+        	result.error = error;
+			return res.json(result);
+		}
+
+        var $ = cheerio.load(html);
+
+        var $result = $('.content').children('strong');
+
+        if ($result.length > 0)
+	        result.data = $result.text();
+    	else 
+    		result.error = 'Wrong Parameters';
+    	
+        return res.json(result);
+    });
+});
+
+var server = app.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('App listening at http://%s:%s', host, port);
+});
